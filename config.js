b715ac4f80d6c79d408eaf9d76de1df472cb1923
Leomar Alejar <leomaramieldev@gmail.com>
2018-02-13 22:21:20 +0800

    Add config.js for passing test suite in bitrise

---
+++ config.js
@@ -0,0 +1,9 @@
+
+export default firebaseConfig = {
+	apiKey: "AIzaSyBpkgAPOPsQxxz7onF9xAQu-m4BuHi3bfA",
+	authDomain: "manageemployeeproject.firebaseapp.com",
+	databaseURL: "https://manageemployeeproject.firebaseio.com",
+	projectId: "manageemployeeproject",
+	storageBucket: "",
+	messagingSenderId: "987983352028"
+}
