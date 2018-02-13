5e359b54c24fbac6efc6d85046fe254654c3d117
Leomar Alejar <leomaramieldev@gmail.com>
2018-02-14 00:00:41 +0800

    Add addListener and navigation Middleware to App.js

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { View, StatusBar } from 'react-native';
+
 import { addNavigationHelpers } from 'react-navigation';
+import { createReduxBoundAddListener, createReactNavigationReduxMiddleware } from 'react-navigation-redux-helpers';
+
 
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
@@ -9,9 +12,15 @@ import mainReducer from './src/reducers';
 
 import Navigator from './src/components/Navigator';
 
+const navigationMiddleware = createReactNavigationReduxMiddleware(
+  "root",
+  state => state.nav,
+);
+const addListener = createReduxBoundAddListener("root");
+
 const NavigatorWithHelpers = ({dispatch, nav}) => (
 	<Navigator navigation={addNavigationHelpers({
-		dispatch, state: nav
+		dispatch, state: nav, addListener
 	})}/>
 )
 
@@ -24,7 +33,7 @@ const ConnectedNavigator = connect(mapStateToProps)(NavigatorWithHelpers);
 export default class App extends Component {
 
   render(){
-    let store = createStore(mainReducer, applyMiddleware(thunk));
+    let store = createStore(mainReducer, applyMiddleware(navigationMiddleware), applyMiddleware(thunk));
     return (
       <Provider store={store}>
         <View style={{flex: 1}}>
